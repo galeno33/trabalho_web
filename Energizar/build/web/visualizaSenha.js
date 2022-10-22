3f123c6fa3b8d11bd31ae60a14656f8c848ebd71
Fabio Galeno <48499860+galeno33@users.noreply.github.com>
2022-10-22 19:38:14 -0300

    Add files via upload

---
+++ Energizar/build/web/visualizaSenha.js
@@ -0,0 +1,16 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+function mostrarSenha() {
+    var senha = document.getElementById("senha");
+    var confirme = document.getElementById("confirme");
+    if (senha.type == "password" && confirme.type == "password") {
+        senha.type = "text";
+         confirme.type = "text";
+    } else {
+        senha.type = "password";
+        confirme.type = "password";
+    }
+}
